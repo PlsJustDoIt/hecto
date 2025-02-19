6ebc432eaf946969ed58582fc021babbfb702a3b
Léo Maugeri <leomaugeri@hotmail.fr>
2025-02-19 22:30:21 +0100

    refactor: enhance editor functionality with terminal integration and event handling

--- src/editor.rs
+++ src/editor.rs
@@ -1,34 +1,73 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+use crate::terminal::Terminal; // If you want to use Terminal directly
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+
 
 pub struct Editor {
-
+    should_quit:bool,
 }
 
 impl Editor {
-    pub fn default() -> Self {            
-        Editor{}            
+    pub const fn default() -> Self {            
+        Self { should_quit: false }            
     }
-    pub fn run(&self){
-        enable_raw_mode().unwrap();
+    pub fn run(&mut self) {
+        Terminal::initialize().unwrap();
+        let result = self.repl();
+        Terminal::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    match (event.code) {
-                        Char(c) => {
-                            // ok
-                            if c == 'q' {
-                                break;
-                            }
-                        },
-                        _ => (),   
-                    }   
-                },
-                Err(err) => println!("broken : {}", err),
-                _ => () // anything else
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+        }   
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                
+                _ => (),
             }
         }
-        disable_raw_mode().unwrap();
     }
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            print!("Goodbye.\r\n");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
+        }
+        Ok(())
+    }
+
+    fn draw_rows() -> Result<(), std::io::Error> {
+        
+        let  rows = Terminal::size()?.1;
+        for n in 0..rows {
+            // Terminal::move_cursor_to(0,n).unwrap(); // ma tech à la base
+            
+            print!("~");
+            if n + 1 < rows {
+                print!("\r\n");
+            }
+        }
+        Ok(())
+
+    }
+
 }
